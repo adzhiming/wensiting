d1124f0bbfa382cf260e3659519bf73053c8f2f7
13710332006 <2013super@sina.com>
2020-05-12 22:29:07 +0800

    no message

--- app/admin/view/index/index.php
+++ app/admin/view/index/index.php
@@ -28,8 +28,9 @@
             <ul class="nav" id="side-menu">
                 <li class="nav-header">
                     <div class="dropdown profile-element admin_open">
-                        <span>
-                            <img alt="image" class="imgbox" src="{$site_logo}" onerror="javascript:this.src='{__ADMIN_PATH}images/admin_logo.png';"/>
+                        <span style="height:60px;line-height: 60px;color: #fff;text-align: center;font-size: 20px;">
+                            文思庭管理后台
+                            <!--<img alt="image" class="imgbox" src="{$site_logo}" onerror="javascript:this.src='{__ADMIN_PATH}images/admin_logo.png';"/>-->
                         </span>
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="clear" style="margin-top: 20px;">
